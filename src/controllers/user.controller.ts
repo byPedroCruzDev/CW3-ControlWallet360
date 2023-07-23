06068979f2df9627355c9ca9b1e58d3bac2ca628
PedroCruz <contatocreed67@gmail.com>
2023-07-23 19:24:50 -0400

    init

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import UserService from "../services/user.service";
+
+class UsersController {
+  static async create() {}
+  static async listAll() {}
+  static async listOne() {}
+  static async update() {}
+  static async delete() {}
+}
---
+++ src/services/user.services.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+
+class UsersServices {
+  static async create() {}
+  static async listAll() {}
+  static async listOne() {}
+  static async update() {}
+  static async delete() {}
+}
